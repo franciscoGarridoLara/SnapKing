3e3c58476e733c816e9db5b40d9b7a4387b379b1
franciscoGarridoLara <francisco.garrido.tec@gmail.com>
2022-02-06 16:29:47 +0100

    MainActivity.kt -> 06/02/22.

--- app/src/main/java/com/example/snapking/MainActivity.kt
+++ app/src/main/java/com/example/snapking/MainActivity.kt
@@ -7,5 +7,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    fun pruebas() : String
+    {
+    return ""
+    }
+
     }
 }
