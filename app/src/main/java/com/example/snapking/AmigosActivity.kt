96641d013aef19ef9d566defe452bef611ae10e6
franciscoGarridoLara <francisco.garrido.tec@gmail.com>
2022-02-15 17:52:26 +0100

    Creación del layout amigos, retoque de otras actividades.

---
+++ app/src/main/java/com/example/snapking/AmigosActivity.kt
@@ -0,0 +1,11 @@
+package com.example.snapking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AmigosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_amigos)
+    }
+}
