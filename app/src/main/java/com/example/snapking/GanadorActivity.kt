059d1897479650f8508b45c8bcc6236fb2f5e0db
franciscoGarridoLara <francisco.garrido.tec@gmail.com>
2022-03-03 21:22:41 +0100

    Layout y cosas Ganador ACti

--- app/src/main/java/com/example/snapking/GanadorActivity.kt
+++ app/src/main/java/com/example/snapking/GanadorActivity.kt
@@ -7,6 +7,7 @@ import com.example.snapking.BaseDatos.BaseDatos
 import com.example.snapking.BaseDatos.IGetJugadorGanador
 import com.example.snapking.BaseDatos.IGetJugadoresFromSala
 import com.example.snapking.Wrapper.WrapperUsuarioPartida
+import com.example.snapking.databinding.ActivityGanadorBinding
 import com.example.snapking.modelo.Jugador
 import com.example.snapking.modelo.WrapperSala
 import com.google.gson.Gson
@@ -14,33 +15,39 @@ import com.google.gson.reflect.TypeToken
 import java.lang.reflect.Type
 
 class GanadorActivity : AppCompatActivity() {
-    private lateinit var  wraperSala:WrapperSala
+    private lateinit var binding: ActivityGanadorBinding
+    private lateinit var wraperSala: WrapperSala
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ganador)
+        binding = ActivityGanadorBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
 
         cogerWrapperSala()
+        setListeners()
 
-        BaseDatos.getInstance()?.getGanadorFromSala(wraperSala.id,object:IGetJugadorGanador{
+        BaseDatos.getInstance()?.getGanadorFromSala(wraperSala.id, object : IGetJugadorGanador {
             override fun oncallBack(ganador: WrapperUsuarioPartida) {
 
             }
 
         })
 
+    }
 
-
-
+    private fun setListeners() {
+        binding.btnLobby.setOnClickListener {
+            startActivity(Intent(this, PrincipalActivity::class.java))
+            finish()
+        }
     }
 
     override fun onBackPressed() {
 
-        startActivity(Intent(this,PrincipalActivity::class.java))
+        startActivity(Intent(this, PrincipalActivity::class.java))
         finish()
 
         //super.onBackPressed() // optional depending on your needs
     }
-
 
     private fun cogerWrapperSala() {
         val intent = intent
